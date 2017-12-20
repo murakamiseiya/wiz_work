d027d2983df7340a8a53f338a3481224bfba0baa
Seiya_Murakami <murakamiseiya1992@gmail.com>
2017-12-20 11:47:21 +0900

    new Project tomproj

---
+++ src/javaBeans/Context.java
@@ -0,0 +1,9 @@
+
+//クラス：コンテキストクラス
+//用途　：定数を設定するクラス
+interface Context implements Serializable{
+
+
+
+
+}
---
+++ src/javaBeans/tmoBeans.java
@@ -0,0 +1,6 @@
+
+//JavaBeansクラス
+//Jspコンテナの全てのページで参照するクラス
+class tomBeans implements Context {
+
+}
